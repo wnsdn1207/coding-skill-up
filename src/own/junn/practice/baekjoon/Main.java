a2a06dac200b00d43fb92513a96fc7724fa7018c
Junn <wnsdn1207@naver.com>
2021-08-31 22:38:51 +0900

    [Baekjoon] 2798

--- src/own/junn/practice/baekjoon/Main.java
+++ src/own/junn/practice/baekjoon/Main.java
@@ -9,10 +9,45 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        String input = reader.readLine();
-        System.out.println(new Main().question_2920(input));
+        String input1 = reader.readLine();
+        String input2 = reader.readLine();
+
+//        System.out.println(new Main().question_2920(input1));
+        System.out.println(new Main().question_2798(input1, input2));
     }
 
+    /**
+     * [Baekjoon] 2798, 블랙잭
+     */
+    private int question_2798(String firstInput, String secondInput) {
+        int answer = 0;
+
+        String[] firstInfo = firstInput.split(" ");
+        int length = Integer.parseInt(firstInfo[0]);
+        int max = Integer.parseInt(firstInfo[1]);
+
+        int[] cards = Arrays.stream(secondInput.split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        for (int i=0; i<length; i++) {
+            for (int j=i+1; j<length; j++) {
+                for (int k=j+1; k<length; k++) {
+                    int total = cards[i] + cards[j] + cards[k];
+
+                    if (total <= max) {
+                        if ((max - answer) > (max - total)) {
+                            answer = total;
+                        }
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    /**
+     * [Baekjoon] 2920, 음계
+     */
     private String question_2920(String scale) {
         int[] scales = Arrays.stream(scale.split(" ")).mapToInt(Integer::parseInt).toArray();
         boolean isAsc = true, isDesc = true;
