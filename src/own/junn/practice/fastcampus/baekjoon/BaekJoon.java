6ff024894afc6ce0a2ed5be75d867f43495e4762
Junn <wnsdn1207@naver.com>
2021-08-31 09:31:35 +0900

    [Fast Campus] baekjoon 2920

---
+++ src/own/junn/practice/fastcampus/baekjoon/BaekJoon.java
@@ -0,0 +1,13 @@
+package own.junn.practice.fastcampus.baekjoon;
+
+public class BaekJoon {
+    public static void main(String[] args) {
+
+    }
+
+    private String question_2920(String[] scales) {
+
+
+        return "";
+    }
+}
