ed4998f759775dda7694e24ebab8348d7d8672be
Junn <wnsdn1207@naver.com>
2021-08-31 22:04:36 +0900

    [Baekjoon] 2920 - 2

---
+++ src/own/junn/practice/baekjoon/Main.java
@@ -0,0 +1,36 @@
+package own.junn.practice.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = reader.readLine();
+        System.out.println(new Main().question_2920(input));
+    }
+
+    private String question_2920(String scale) {
+        int[] scales = Arrays.stream(scale.split(" ")).mapToInt(Integer::parseInt).toArray();
+        boolean isAsc = true, isDesc = true;
+
+        for (int i=1; i<scales.length; i++) {
+            if (scales[i] >= scales[i-1]) {
+                isDesc = false;
+            } else if (scales[i] < scales[i-1]) {
+                isAsc = false;
+            }
+        }
+
+        if (isAsc) {
+            return "ascending";
+        } else if (isDesc) {
+            return "descending";
+        } else {
+            return "mixed";
+        }
+    }
+}
